50f7e0165294757f5686954c73fa5f2908158106
sadflask <benjamin.n.frew@gmail.com>
2016-10-07 14:47:01 +1300

    Made simple script to move person.

---
+++ Navma'am/Assets/Scripts/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonController : MonoBehaviour {
+    public float walkSpeed;
+    public float turnSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {    
+	    if((Input.GetAxis("Horizontal")) < 0)
+        {
+            //Turn left (rotate in -y)
+            transform.Rotate(-Vector3.up * Time.deltaTime * turnSpeed);
+
+        } else if((Input.GetAxis("Horizontal")) > 0)
+        {
+            //Turn right (rotate in +y)
+            transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed);
+        }
+        if((Input.GetAxis("Vertical") > 0))
+        {
+            //Move forward (translate along local x-axis)
+            transform.Translate(Vector3.forward * Time.deltaTime * walkSpeed );
+        }
+	}
+}
