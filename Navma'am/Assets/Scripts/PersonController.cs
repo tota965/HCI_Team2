4e4246a3c77e1b6700dfc5899fb05992b7729ccb
sadflask <benjamin.n.frew@gmail.com>
2016-10-11 11:18:05 +1300

    Added controller using L + R hands, integrated Leap.

--- Navma'am/Assets/Scripts/PersonController.cs
+++ Navma'am/Assets/Scripts/PersonController.cs
@@ -4,24 +4,27 @@ using System.Collections;
 public class PersonController : MonoBehaviour {
     public float walkSpeed;
     public float turnSpeed;
-	// Use this for initialization
+    // Use this for initialization
+    public bool moveForward;
+    public bool turnLeft;
+    public bool turnRight;
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {    
-	    if((Input.GetAxis("Horizontal")) < 0)
+	    if(turnLeft)
         {
             //Turn left (rotate in -y)
             transform.Rotate(-Vector3.up * Time.deltaTime * turnSpeed);
 
-        } else if((Input.GetAxis("Horizontal")) > 0)
+        } else if(turnRight)
         {
             //Turn right (rotate in +y)
             transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed);
         }
-        if((Input.GetAxis("Vertical") > 0))
+        if((moveForward))
         {
             //Move forward (translate along local x-axis)
             transform.Translate(Vector3.forward * Time.deltaTime * walkSpeed );
